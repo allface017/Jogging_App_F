11dcaaf9bdab38014341ba5788dd397fbbfd1fed
renka <r.oomori.sys22@morijyobi.ac.jp>
2024-03-22 09:45:23 +0900

    データ一覧途中 #6

---
+++ app/Models/Spot_lists.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Spot_lists extends Model
+{
+    use HasFactory;
+    protected $guarded = ['id'];
+
+    public function spots(){
+        return $this->belongsTo('App\Models\Spots');
+    }
+    // public function spots(){
+    //     return $this->hasMany(Spots::class, 'post_number');
+    // }
+}
