800bc4dc71bf7c821470eb96ec2cd5f42529ba1f
yuzuki <y.yamahata.sys23@morijyobi.ac.jp>
2024-03-26 10:17:24 +0900

    jogging_add.blade.phpのbrタグの削除

--- app/Http/Controllers/JoggingController.php
+++ app/Http/Controllers/JoggingController.php
@@ -72,4 +72,53 @@ class JoggingController extends Controller
         }
         return redirect('/jogging');
     }
+
+    //目標設定表示
+    public function target_index(){
+        $user = Auth::id();
+        //全体走行距離を取得
+        $total_distance = Jogs::where('users_id',$user)->sum('distance');
+        $user_id = Auth::id();
+        // 達成されていない目標を更新
+        $this->updateTargetAchievement($user_id);
+        // 再度達成されていない目標を取得
+        $target = Targets::where('users_id', $user_id)
+        ->where('achieveflg', false)
+        ->where('deleteflg', false)
+        ->first();
+        $target_list = Targets::where('users_id',$user)->where('deleteflg',false)->get();   
+
+        return view('jogging.target_sett',['total' => $total_distance,'target' =>$target,'target_list'=>$target_list]);
+
+    }
+    //目的追加
+    public function target_add(Request $request){
+        $target = new Targets;
+        $target->users_id = Auth::id();
+        $target->target_distance = $request->target_distance;
+        $target->reward = $request->reward;
+        $target->created_at = now();
+        $target->updated_at = now();    
+        $target->deleteflg = false;
+        $target->achieveflg = false;
+        $target->save();
+
+        return redirect('/jogging/target');
+    }
+
+    //目標を更新
+    public function updateTargetAchievement($user_id) {
+        // 全体走行距離を取得
+        $total_distance = Jogs::where('users_id', $user_id)->sum('distance');
+        // 達成されていない目標を取得
+        $target = Targets::where('users_id', $user_id)
+                        ->where('achieveflg', false)
+                        ->where('deleteflg', false)
+                        ->first();
+        // 目標が存在し、目標距離に達していたら更新
+        if ($target && $total_distance >= $target->target_distance) {
+            $target->achieveflg = true;
+            $target->save();
+        }
+    }
 }
