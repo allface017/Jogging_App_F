62ee52a4db6bab94aa10787efb56991910cdfaad
yuzuki <y.yamahata.sys23@morijyobi.ac.jp>
2024-03-26 12:20:47 +0900

    jogging_comfirm.blade.phpのクラス名の変更

--- resources/views/jogging/jogging_comfirm.blade.php
+++ resources/views/jogging/jogging_comfirm.blade.php
@@ -9,73 +9,72 @@
 @section('content')
     
 
-<div class="top-p">
-        <p>以下の内容で登録が完了しました。</p>
-    </div>
+<p class="top-p">以下の内容で登録が完了しました。</p>
+ 
+
+ <div class="form">
+
+     <div class="nitizi">
+         <p class="fo-si-32">日時</p>
+
+         <p id="nitizi"></p>
+
+     </div>
 
 
-    <div class="form">
-
-        <div class="日時">
-            <p class="fo-si-32">日時</p>
-
-            <p id="nitizi"></p>
-
-        </div>
+     <div class="kyori">
+         <p class="fo-si-32">距離(km)</p>
 
 
-        <div class="距離">
-            <p class="fo-si-32">距離(km)</p>
+         <p id="kyori"></p>
 
-            <p id="kyori"></p>
-
-        </div>
+     </div>
 
 
-        <div class="運動時間">
-            <p class="fo-si-32">運動時間</p>
+     <div class="runtime">
+         <p class="fo-si-32">運動時間</p>
 
-            <p id="runtime"></p>
+         <p id="runtime"></p>
 
-        </div>
-
-
-        <div class="運動場所">
-            <p class="fo-si-32" >運動場所</p>
-            <div class="外か内">
-
-                <p id="外">外</p>
-
-                <p id="内">内</p>
-
-            </div>
-
-        </div>
+     </div>
 
 
-        <div class="ジョギングコースの画像">
-            <p class="fo-si-32">ジョギングコースの画像</p>
+     <div class="space">
+         <p class="fo-si-32" >運動場所</p>
+         <div class="out-in">
 
-            <img src="#" alt="ジョギングコースの画像" id="jogging-img">
+             <p id="out">外</p>
 
-        </div>
+             <p id="in">内</p>
 
+         </div>
 
-        <div class="スポット">
-            <p class="fo-si-32">スポット</p>
-
-            <ul>
-                <li>スポットA</li>
-            </ul>
-
-        </div>
-
-    </div>
+     </div>
 
 
-    <div id="戻るリンク">
-        <a href="#">戻る</a>
-    </div>
+     <div class="jogging-img">
+         <p class="fo-si-32">ジョギングコースの画像</p>
+
+         <img src="#" alt="ジョギングコースの画像" id="jogging-img">
+
+     </div>
+
+
+     <div class="spot">
+         <p class="fo-si-32">スポット</p>
+
+         <ul>
+             <li>スポットA</li>
+         </ul>
+
+     </div>
+
+ </div>
+
+
+ <div id="return">
+     <a href="#">戻る</a>
+ </div>
 
 @endsection
 
