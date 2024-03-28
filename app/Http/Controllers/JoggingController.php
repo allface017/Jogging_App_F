df9db69f63202fc9f7badb4db510a4d7df27b895
yuzuki <y.yamahata.sys23@morijyobi.ac.jp>
2024-03-28 11:53:05 +0900

    jogging_addのcssの修正

--- app/Http/Controllers/JoggingController.php
+++ app/Http/Controllers/JoggingController.php
@@ -51,9 +51,9 @@ class JoggingController extends Controller
     }
     // ジョギングデータ登録
     public function jogging_add(){
-        // $spots = Spots::all();
-        // return view('jogging.jogging_add',['spots'=>$spots]);
-        return view('jogging.jogging_add');
+        $spots = Spots::all();
+        return view('jogging.jogging_add',['spots'=>$spots]);
+        // return view('jogging.jogging_add');
     }
     public function jogging_create(Request $request){
         $jogs = new Jogs;
