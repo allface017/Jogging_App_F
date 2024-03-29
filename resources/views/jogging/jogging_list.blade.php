800b5a69bee23eb8974781e6d53c6674ba6b4e93
renka <r.oomori.sys22@morijyobi.ac.jp>
2024-03-29 09:58:37 +0900

    絞り込み機能途中 #6

--- resources/views/jogging/jogging_list.blade.php
+++ resources/views/jogging/jogging_list.blade.php
@@ -41,18 +41,39 @@
         </div>
 
         <div class="search-menu flex">
-            <a href="#" id="narrow-down" class="flex">  
+            <!-- <a href="#" id="narrow-down" class="flex">  
                 <span class="material-symbols-outlined">page_info</span>
-                <!-- <p>絞り込み</p> -->
-                <p><button type="button" onclick="document.getElementById('ex-dialog-3').showModal()">絞り込み</button></p>
-            </a>
-            <dialog id="ex-dialog-3" aria-labelledby="ex-dialog-3-title">
-            <form method="dialog">
-                <h3 id="ex-dialog-3-title">名前を入力</h3>
-                <p><label>名前: <input type="text" required></label></p>
+                <p>絞り込み</p>
+            </a> -->
+            <input type="checkbox" id="find_check" checked>
+            <label class="find_check" for="find_check">
+                絞り込み
+            </label>
+            <form action="#" method="post" class="find_form">
+                <h3 id="ex-dialog-3-title">日付</h3>
+                <label><input type="radio" name="date" value="年月" checked="">年月</label>
+                <label><input type="radio" name="date" value="年月日" checked="">年月日</label>
+
+                <h3 id="ex-dialog-3-title">距離</h3>
+                <div class="min-bar">
+                    <p>最小：<span id="min-distance-value">0</span>km</p>
+                    <input type="range" name="min_distance" id="min-distance" onchange="updateMinValue()" value="{{ $form['min_distance'] ?? '' }}">
+                </div>
+                <div class="max-bar">
+                    <p>最大：<span id="max-distance-value">0</span>km</p>
+                    <input type="range" name="max_distance" id="max-distance" onchange="updateMaxValue()" value="{{ $form['max_distance'] ?? '' }}">
+                </div>
+
+                <h3 id="ex-dialog-3-title">場所</h3>
+                <label><input type="radio" name="location" value="外" checked="">外</label>
+                <label><input type="radio" name="location" value="内" checked="">内</label>
+
+                <h3 id="ex-dialog-3-title">スポット</h3>
+                <label><input type="checkbox" name="spots[]" value="スポット１" id="1">スポット１</label>
+                <label><input type="checkbox" name="spots[]" value="スポット２" id="2">スポット２</label>
+                <label><input type="checkbox" name="spots[]" value="スポット３" id="3">スポット３</label>
                 <p><button type="submit">確定する</button></p>
             </form>
-            </dialog>
             <a href="#" id="registration-order">登録順</a>
             <a href="#" id="">昇順</a>
         </div>
